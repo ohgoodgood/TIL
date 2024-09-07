be372618c74b96d23103d35e69f17cfee006a6cb
ohgoodgood <jskim9504@gmail.com>
2024-09-07 17:58:46 +0900

    second workout

---
+++ JavaScript/workspace/02-javascript-basic/02-1-getting-started/test2-outofhtml/main.js
@@ -0,0 +1,17 @@
+// const myButtonClick = () => {
+//     let title = document.getElementById('title')
+//     if(title.style.backgroundColor === 'yellow'){
+//       title.style.backgroundColor = 'pink'
+//     }else {
+//       title.style.backgroundColor = 'yellow'
+//     }
+//   }
+
+const myButtonClick = () => {
+    let title = document.getElementById('title')
+    if(title.style.backgroundColor === 'yellow'){
+        title.style.backgroundColor = 'pink'
+    } else {
+        title.style.backgroundColor = 'yellow'
+    }
+}
