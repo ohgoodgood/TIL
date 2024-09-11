58fc14818f0dc796d975235c40c784e285ea0a66
ohgoodgood <jskim9504@gmail.com>
2024-09-11 17:44:35 +0900

    trim dir structure and add til

---
+++ complete-javascript-course/01-Fundamentals-Part-1/practices/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda"; // 여기서 값을 바꿔주면 같은 변수가 사용된 모든 곳에 적용되어 편리하다
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
