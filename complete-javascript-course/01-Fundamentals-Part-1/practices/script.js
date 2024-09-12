6ea09efd0487a6149c13b427027848ac4fbc17f8
ohgoodgood <jskim9504@gmail.com>
2024-09-12 11:39:13 +0900

    up to lecture 13

--- complete-javascript-course/01-Fundamentals-Part-1/practices/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/practices/script.js
@@ -26,7 +26,7 @@ console.log(myFirstJob);
 -- up to chapter 10 --
 */
 
-/* chapter 11
+/*
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -45,8 +45,11 @@ console.log(typeof year);
 
 year = 1991;
 console.log(typeof year);
+
+-- up to chapter 11 --
 */
 
+/*
 let age = 30;
 age = 31; // 'reassigning a value to a variable' or 'mutating a variable'
 
@@ -61,4 +64,35 @@ job = 'teacher';
 /* This is a poor idea
 lastName = 'Kim';
 console.log(lastName);
+
+-- up to chapter 12 --
 */
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
