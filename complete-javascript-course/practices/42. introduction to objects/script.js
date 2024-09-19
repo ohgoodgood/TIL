181dcf65887702a8af8be973ff68d47cbd4251c7
ohgoodgood <jskim9504@gmail.com>
2024-09-19 13:31:02 +0900

    up to lecture 42

---
+++ complete-javascript-course/practices/42. introduction to objects/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// an object is another type of data structure in JS.
+
+// object literal syntax
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+// firstName, lastName, age, job, friends: properties
+// 'Jonas', 'Schmedtmann' ... : property values
