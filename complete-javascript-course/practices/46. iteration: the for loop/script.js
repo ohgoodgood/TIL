c9dd61fb741a99e4998446ad0a82ed5e606a0453
ohgoodgood <jskim9504@gmail.com>
2024-09-19 17:16:04 +0900

    up to lecture 46

---
+++ complete-javascript-course/practices/46. iteration: the for loop/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+console.log('Lifting weights repetition 1 🏋🏻‍♀️');
+console.log('Lifting weights repetition 2 🏋🏻‍♀️');
+console.log('Lifting weights repetition 3 🏋🏻‍♀️');
+console.log('Lifting weights repetition 4 🏋🏻‍♀️');
+console.log('Lifting weights repetition 5 🏋🏻‍♀️');
+console.log('Lifting weights repetition 6 🏋🏻‍♀️');
+console.log('Lifting weights repetition 7 🏋🏻‍♀️');
+console.log('Lifting weights repetition 8 🏋🏻‍♀️');
+console.log('Lifting weights repetition 9 🏋🏻‍♀️');
+console.log('Lifting weights repetition 10 🏋🏻‍♀️');
+*/
+
+// for loop keeps running while the condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
+}
