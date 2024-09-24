e7c548868ad525fcb186a4bce2e6f8a07b812e4f
ohgoodgood <jskim9504@gmail.com>
2024-09-24 11:52:29 +0900

    challenge1_again_button

--- complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_1_guessmynumber/script.js
+++ complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_1_guessmynumber/script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; // 데이터를 DOM에서 읽어오는 방식도 가능하지만, 그보다는 코드 내에 데이터를 작성해 주는 것이 좋다! 데이터를 DOM에 전적으로 의존해서 확인하는 것보다는 코드 자체에서 확인(참조)할 수 있는 편이 좋음.
-document.querySelector(`.number`).textContent = secretNumber;
 
 // in EventListener method, 1st argument is the event that we're wating for and 2nd argument is a function that shd be executed when the event happens.
 // the function in EventListener is not called by us. it's called by JavaScript engine as soon as the event occurs.
@@ -17,6 +16,8 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `Correct Number!`;
+
+    document.querySelector(`.number`).textContent = secretNumber;
 
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
 
@@ -46,4 +47,27 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.score`).textContent = 0;
     }
   }
+});
+
+/* when user clicked 'again' button - my approach
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  document.querySelector(`.score`).textContent = 20;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.message`).textContent = `Start Guessing...`;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
+});
+*/
+
+// lecture approach
+document.querySelector(`.agian`).addEventListener(`click`, function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
 });
