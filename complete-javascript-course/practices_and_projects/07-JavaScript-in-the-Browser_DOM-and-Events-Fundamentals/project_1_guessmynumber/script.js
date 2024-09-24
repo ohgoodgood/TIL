1b9d9acaa6d83c8b947123ef61d33c2fdfb7ae9b
ohgoodgood <jskim9504@gmail.com>
2024-09-24 13:34:50 +0900

    implemented highscore functionality

--- complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_1_guessmynumber/script.js
+++ complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_1_guessmynumber/script.js
@@ -2,10 +2,11 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; // 데이터를 DOM에서 읽어오는 방식도 가능하지만, 그보다는 코드 내에 데이터를 작성해 주는 것이 좋다! 데이터를 DOM에 전적으로 의존해서 확인하는 것보다는 코드 자체에서 확인(참조)할 수 있는 편이 좋음.
+let highscore = 0;
 
-// in EventListener method, 1st argument is the event that we're wating for and 2nd argument is a function that shd be executed when the event happens.
-// the function in EventListener is not called by us. it's called by JavaScript engine as soon as the event occurs.
 document.querySelector(`.check`).addEventListener(`click`, function () {
+  // in EventListener method, 1st argument is the event that we're wating for and 2nd argument is a function that shd be executed when the event happens.
+  // the function in EventListener is not called by us. it's called by JavaScript engine as soon as the event occurs.
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
 
@@ -22,8 +23,13 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
 
     document.querySelector(`.number`).style.width = `30rem`;
-
     // 자바스크립트로 css를 조정하면 html 문서에 in-line style로 입력된다! 우선순위 때문인가?
+
+    // highscore functionality
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(`.highscore`).textContent = highscore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
@@ -49,7 +55,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
   }
 });
 
-/* when user clicked 'again' button - my approach
+/* 'again' button - my approach
 document.querySelector(`.again`).addEventListener(`click`, function () {
   document.querySelector(`.score`).textContent = 20;
   document.querySelector(`.number`).textContent = `?`;
@@ -59,8 +65,8 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
 });
 */
 
-// lecture approach
-document.querySelector(`.agian`).addEventListener(`click`, function () {
+// 'again' button - lecture approach
+document.querySelector(`.again`).addEventListener(`click`, function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
