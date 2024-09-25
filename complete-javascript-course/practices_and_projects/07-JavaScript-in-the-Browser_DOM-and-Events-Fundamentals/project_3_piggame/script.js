ecc6aec1313d85e591dc68042b7d2326b2a5c312
ohgoodgood <jskim9504@gmail.com>
2024-09-25 14:13:30 +0900

    coding challenge done, project complete

--- complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_3_piggame/script.js
+++ complete-javascript-course/practices_and_projects/07-JavaScript-in-the-Browser_DOM-and-Events-Fundamentals/project_3_piggame/script.js
@@ -15,37 +15,79 @@ const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
 
 // starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add(`hidden`);
+let scores, currentScore, activePlayer, playing; // declaring at global scope
 
-const scores = [0, 0];
-let currentScore = 0; // this should be outside of the function below!
-let activePlayer = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true; // 게임이 끝났는지 진행중인지에 대한 변수. true/false 값에 따라 logic이 실행되는지 아닌지를 간편하게 결정할 수 있다.
+
+  diceEl.classList.add(`hidden`);
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  document.getElementById(`current--0`).textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
+
+  document.querySelector(`.player--${activePlayer}`).classList.remove(`player--winner`);
+  document.querySelector(`.player--${activePlayer}`).classList.remove(`player--active`);
+  document.querySelector(`.player--0`).classList.add(`player--active`);
+};
+init();
+
+// switch player function
+const switchPlayer = function () {
+  // switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+
+  // toggle method
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+};
 
 // rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
-  // 1. generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6 + 1);
+  if (playing) {
+    // 1. generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6 + 1);
 
-  // 2. display dice
-  diceEl.classList.remove(`hidden`);
-  diceEl.src = `dice-${dice}.png`;
+    // 2. display dice
+    diceEl.classList.remove(`hidden`);
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. check for rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    // add dice to the current score
-    //currentScore = currentScore + dice;
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-  } else {
-    // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-
-    // toggle method
-    player0El.classList.toggle(`player--active`);
-    player1El.classList.toggle(`player--active`);
+    // 3. check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // add dice to the current score
+      //currentScore = currentScore + dice;
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+// score hold functionality
+btnHold.addEventListener(`click`, function () {
+  if (playing) {
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    // 2. check if player's score is >= 10 and if so, finish the game
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add(`hidden`);
+      document.querySelector(`.player--${activePlayer}`).classList.add(`player--winner`);
+      document.querySelector(`.player--${activePlayer}`).classList.remove(`player--active`);
+    } else {
+      // switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+// reset(new game) button functionality
+btnNew.addEventListener(`click`, init);
