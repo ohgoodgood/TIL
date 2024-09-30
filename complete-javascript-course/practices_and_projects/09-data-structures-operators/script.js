2703b2b7ddb50d64fb8a97dbba4abc6f21693a2e
ohgoodgood <jskim9504@gmail.com>
2024-09-30 15:13:56 +0900

    maps_iteration

--- complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
+++ complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
@@ -248,7 +248,44 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
-////////// Maps //////////
+////////// Maps: Iteration //////////
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+// boolean도 (또는 어떤 타입의 데이터도) key로 삼을 수 있어서 가능한 코드!
+console.log(question.get(question.get('correct') === answer));
+
+// convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+////////// Maps: Fundamentals //////////
+/*
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
@@ -286,6 +323,7 @@ console.log(rest.get(arr));
 // now it works
 
 rest.set(document.querySelector('h1'), 'Heading');
+*/
 
 ////////// Sets //////////
 /*
