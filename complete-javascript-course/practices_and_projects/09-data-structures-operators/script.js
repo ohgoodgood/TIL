910c092eab7a77764abee12c658c891f8e8b518b
ohgoodgood <jskim9504@gmail.com>
2024-09-30 19:25:29 +0900

    working with strings part 2

--- complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
+++ complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
@@ -293,7 +293,67 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
+////////// Working with strings Part 2 //////////
+const airLine = 'TAP Air Portugal';
+
+console.log(airLine.toLocaleLowerCase());
+console.log(airLine.toLocaleUpperCase());
+
+// Fix capitalization in names
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97￡';
+const priceUS = priceGB.replace('￡', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase(); // 전부 다 소문자로 바꿔 놓고 대조해야지, 안그러면 모든 경우의 수 다 대조해야함
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
 ////////// Working with strings Part 1 //////////
+/*
 const airLine = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -336,6 +396,7 @@ checkMiddleSeat('3E');
 console.log(new String('jonas'));
 console.log(typeof new String('jonas'));
 console.log(typeof new String('jonas').slice(1));
+*/
 
 ////////// Which data structure to use? //////////
 
