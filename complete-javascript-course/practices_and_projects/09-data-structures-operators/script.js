d0178207c2306e3e6657f351b66f5d8c3a491e63
ohgoodgood <jskim9504@gmail.com>
2024-10-01 14:15:03 +0900

    string methods practice

--- complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
+++ complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
@@ -2,10 +2,6 @@
 
 //////////////////////////////////////// LECTURE ////////////////////////////////////////
 
-////////// Data needed for a later exercise //////////
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 ////////// Data needed for first part of the section //////////
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -75,6 +71,32 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 21',
   starterIndex: 1,
 });
+
+////////// String Methods Practice //////////
+
+// DATA //
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// GOAL //
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
+  console.log(output);
+}
 
 ////////// Working with strings Part 3 //////////
 /*
@@ -720,6 +742,7 @@ HINT 3: Start without worrying about the ✅. Tackle that only after you have th
 HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
 */
 
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -738,6 +761,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20, ' ')}${`✅`.repeat(i + 1)}`);
   }
 });
+*/
 
 //////////////////// Coding Challenge #3 ////////////////////
 /*
