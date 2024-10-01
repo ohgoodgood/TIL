f15ecaa4e1e6b73f66dcdb023c8448ac8fae8735
ohgoodgood <jskim9504@gmail.com>
2024-10-01 09:42:06 +0900

    working with strings part 3

--- complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
+++ complete-javascript-course/practices_and_projects/09-data-structures-operators/script.js
@@ -293,7 +293,61 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
+////////// Working with strings Part 3 //////////
+
+// split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // OR
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica and smith davis');
+capitalizeName('jonas schmedtmann');
+
+// padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // string 으로 만들기
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(12345678));
+console.log(maskCreditCard(4337864638637384));
+console.log(maskCreditCard('3817491825475729384619'));
+
+// repeat
+const message2 = 'Bad weather... ALL departures delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${`✈️`.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
 ////////// Working with strings Part 2 //////////
+/*
 const airLine = 'TAP Air Portugal';
 
 console.log(airLine.toLocaleLowerCase());
@@ -351,6 +405,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+*/
 
 ////////// Working with strings Part 1 //////////
 /*
