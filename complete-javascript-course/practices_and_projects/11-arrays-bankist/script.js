eb0c5d51c10fb19e05514cb4211f950182ffb7a9
ohgoodgood <jskim9504@gmail.com>
2024-10-04 15:20:21 +0900

    filter method

--- complete-javascript-course/practices_and_projects/11-arrays-bankist/script.js
+++ complete-javascript-course/practices_and_projects/11-arrays-bankist/script.js
@@ -264,6 +264,28 @@ console.log(movementsDescriptions);
 // NOTE: with for-each method, 'side effects' occur for every iteration. BUT there's no side effect from map method.
 */
 
+////////////////////// the filter method ////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// using filter method. more 'functional' approach and compatible with chaining.
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+// using for-of looping. chaining is not available.
+const depositsFor = [];
+for (const mov of movements)
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+console.log(depositsFor);
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 // coding challenge #1
