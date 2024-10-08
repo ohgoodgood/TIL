dfbc75adb1369825272af432b4368310f56b8af4
ohgoodgood <jskim9504@gmail.com>
2024-10-08 13:18:21 +0900

    timers

--- complete-javascript-course/practices_and_projects/12-numbers-dates-timers-bankist/starter/script.js
+++ complete-javascript-course/practices_and_projects/12-numbers-dates-timers-bankist/starter/script.js
@@ -279,14 +279,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -560,3 +562,22 @@ console.log(
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
 */
+
+/////////////////////////////// timers: setTimeout and setInterval ////////////////////////////////////
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+); // in the background, JS holds 3sec before registering the callback function
+console.log('Waiting...'); // while this is first executed. Asynchronous JavaScript
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
