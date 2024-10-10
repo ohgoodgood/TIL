5340e92d7467942b61a610ccda3d8069a6c57031
ohgoodgood <jskim9504@gmail.com>
2024-10-10 10:44:58 +0900

    DOM traversing

--- complete-javascript-course/practices_and_projects/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/practices_and_projects/13-Advanced-DOM-Bankist/starter/script.js
@@ -264,3 +264,35 @@ h1.addEventListener('mouseenter', alertH1);
 // 이런 것도 가능
 // setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 */
+
+////////////////////////////////// DOM traversing ///////////////////////////////////
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: selecting child elements
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: selecting parent elements
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: selecting siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// example: working with all the siblings
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
