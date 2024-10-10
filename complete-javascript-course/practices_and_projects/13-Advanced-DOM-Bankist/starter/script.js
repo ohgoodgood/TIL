6667a00b004379b3963b8600d2a64ad430a7bd74
ohgoodgood <jskim9504@gmail.com>
2024-10-10 17:09:21 +0900

    revealing elements on scroll

--- complete-javascript-course/practices_and_projects/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/practices_and_projects/13-Advanced-DOM-Bankist/starter/script.js
@@ -179,11 +179,11 @@ window.addEventListener('scroll', function () {
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -196,6 +196,31 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+//// Revealing elements on scroll ////
+
+// reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 //// Event propagation: capturing and bubbling ////
 /*
