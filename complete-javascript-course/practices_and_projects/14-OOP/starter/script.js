f48f5baab839945d3e9d387eedc2650c29fbafbc
ohgoodgood <jskim9504@gmail.com>
2024-11-12 17:30:03 +0900

    coding challenge 3

--- complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
+++ complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
@@ -434,20 +434,24 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
 */
-/*
+
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 };
 
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+console.log(car1, car2);
+
 Car.prototype.accelerate = function () {
   this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
+  console.log(this.speed);
 };
 
 Car.prototype.brake = function () {
   this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
+  console.log(this.speed);
 };
 
 const EV = function (make, speed, charge) {
@@ -455,7 +459,6 @@ const EV = function (make, speed, charge) {
   this.charge = charge;
 };
 
-// linking the prototypes
 EV.prototype = Object.create(Car.prototype);
 
 EV.prototype.chargeBattery = function (chargeTo) {
@@ -464,17 +467,13 @@ EV.prototype.chargeBattery = function (chargeTo) {
 
 EV.prototype.accelerate = function () {
   this.speed += 20;
-  this.charge -= 1;
+  this.charge--;
   console.log(
-    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
   );
 };
 
-const tesla = new EV('Tesla', 120, 23);
-tesla.chargeBattery(90);
-console.log(tesla);
-tesla.brake();
-tesla.accelerate();
-tesla.accelerate();
-tesla.accelerate();
-*/
+const Tesla = new EV('Tesla', 120, 23);
+Tesla.chargeBattery(90);
+Tesla.brake();
+Tesla.accelerate();
