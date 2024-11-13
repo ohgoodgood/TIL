59178a474658f724a5a441a514478a763719f672
ohgoodgood <jskim9504@gmail.com>
2024-11-13 17:50:23 +0900

    ch223

--- complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
+++ complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
@@ -335,7 +335,7 @@ const jacob = new StudentClSimple('Jacob Tiller', 2001);
 */
 
 ///////////////// Inheritance between "classes": (3) Object.create //////////////////
-
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -363,6 +363,52 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+////////////////////////////// Another Class Example ////////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}.`);
+  }
+
+  // Public Interfaces
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
 
 /////////////////////////////////////////////////////////////////////////////////////
 
