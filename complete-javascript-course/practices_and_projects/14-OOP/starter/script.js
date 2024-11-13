b3348f8e576ece4b207b3ea92a68d0f6b00889cc
ohgoodgood <jskim9504@gmail.com>
2024-11-13 16:37:29 +0900

    ch222

--- complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
+++ complete-javascript-course/practices_and_projects/14-OOP/starter/script.js
@@ -336,6 +336,34 @@ const jacob = new StudentClSimple('Jacob Tiller', 2001);
 
 ///////////////// Inheritance between "classes": (3) Object.create //////////////////
 
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
+
 /////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////// Coding Challenge #1 ////////////////////////////////
